73c84272023f0f32f3e28d5586f6466e4d6ee2e2
Bashayer Al-Humaidhi <bashayer@bashayers-macbook-pro.local>
2019-02-02 21:53:53 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Bashayer  on 02/02/2019.
+//  Copyright © 2019 Bashayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
